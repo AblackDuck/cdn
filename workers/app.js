eea61afff655fdc9a3a4e73e670d72dde363ff4e
AblackDuck <bbgmomo200@gmail.com>
2023-06-17 22:45:35 +0700

    dasdasd

--- workers/app.js
+++ workers/app.js
@@ -292,7 +292,7 @@ function list(path) {
     var content = `<div class="container">
   <div id="update"></div>
     <div id="head_md" style="display:none; padding: 20px 20px;"></div>
-    <div class="${UI.path_nav_alert_class} d-flex align-items-center" role="alert" style="margin-bottom: 0; padding-bottom: 0rem;">
+    <div class="${UI.path_nav_alert_class} d-flex align-items-center" role="alert" ">
   <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
     <ol class="breadcrumb" id="folderne"><li class="breadcrumb-item"><a href="/">Home</a></li>`;
     var navlink = '';
